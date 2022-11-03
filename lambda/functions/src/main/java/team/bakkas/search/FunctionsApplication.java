431a53dd2cdab657beec5366a7be7f3909b20f36
BrianDYKim <18k7102dy@naver.com>
2022-11-04 02:43:41 +0900

    [ADD] 각 모듈마다의 시스템 진입점 정의

---
+++ lambda/functions/src/main/java/team/bakkas/search/FunctionsApplication.java
@@ -0,0 +1,12 @@
+package team.bakkas.search;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FunctionsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FunctionsApplication.class, args);
+    }
+}
