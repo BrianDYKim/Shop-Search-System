3987fc0af568fd33f89d1d023c5ddc194c8e7f63
BrianDYKim <18k7102dy@naver.com>
2022-11-03 23:55:21 +0900

    Initial commit

---
+++ src/test/java/team/bakkas/shopSearch/ShopSearchSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package team.bakkas.shopSearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopSearchSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
