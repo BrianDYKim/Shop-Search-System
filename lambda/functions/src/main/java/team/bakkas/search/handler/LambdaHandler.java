0afbc20187c68a92b3b3e14e611056a1241efbae
BrianDYKim <18k7102dy@naver.com>
2022-11-04 16:22:35 +0900

    [ADD] Lambda Handler 정의

---
+++ lambda/functions/src/main/java/team/bakkas/search/handler/LambdaHandler.java
@@ -0,0 +1,8 @@
+package team.bakkas.search.handler;
+
+import org.springframework.cloud.function.adapter.aws.SpringBootStreamHandler;
+
+// AWS Lambda에서 사용하는 Handler class
+public class LambdaHandler extends SpringBootStreamHandler {
+
+}
