76296624030a3e3e7b992940aaaeb4059360a185
BrianDYKim <18k7102dy@naver.com>
2022-11-05 20:27:10 +0900

    [ADD] (lambda) Lambda endpoint 정의 완료

---
+++ lambda/functions/src/main/java/team/bakkas/search/functions/SearchByCategory.java
@@ -0,0 +1,25 @@
+package team.bakkas.search.functions;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import team.bakkas.search.shop.application.ShopService;
+import team.bakkas.search.shop.domain.dto.request.CategoryWithInSearchRequest;
+
+import java.util.List;
+import java.util.function.Function;
+
+/**
+ * @author Doyeop Kim
+ * @version 0.0
+ * @since 2022/11/05
+ */
+@Component
+@RequiredArgsConstructor
+public class SearchByCategory implements Function<CategoryWithInSearchRequest, List<String>> {
+    private final ShopService shopService;
+
+    @Override
+    public List<String> apply(CategoryWithInSearchRequest request) {
+        return shopService.searchByCategoryWithIn(request);
+    }
+}
